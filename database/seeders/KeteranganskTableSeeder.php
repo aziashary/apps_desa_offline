ab939288f54966cf651d3f216e2cf49134cc4ad7
aziashary <harriw4ng@gmail.com>
2024-01-24 16:13:55 +0700

    6 surat

--- database/seeders/KeteranganskTableSeeder.php
+++ database/seeders/KeteranganskTableSeeder.php
@@ -16,21 +16,21 @@ class KeteranganskTableSeeder extends Seeder
     public function run()
     {
         Keterangansk::create([
-            'kode_sk' => 510,
             'id_kodesk' => 1,
+            'kode_sk' => '503',
             'detail_keterangansk' => json_encode([
                 'no_sk' => 'I8',
                 'nik' => 'F14',
                 'warga' => [
                     [
-                    'nama_warga' => 'F13',
-                    'jenis_kelamin' => 'F14',
-                    'tempat_lahir' => 'F15',
-                    'tanggal_lahir' => 'G15',
-                    'alamat' => 'F18',
-                    'jenis_pekerjaan' => 'F16',
-                    'agama' => 'F17',
-                    ],
+                        'nama_warga' => 'F13',
+                        'jenis_kelamin' => 'F14',
+                        'tempat_lahir' => 'F15',
+                        'tanggal_lahir' => 'G15',
+                        'alamat' => 'F18',
+                        'jenis_pekerjaan' => 'F16',
+                        'agama' => 'F17',
+                    ]
                 ],
                 'tanggal' => 'J27',
                 'ttd_kepala' => 'G34',
@@ -39,12 +39,14 @@ class KeteranganskTableSeeder extends Seeder
             'keterangan' => json_encode([
                 'keterangan_1' => 'F22',
                 'keterangan_2' => 'F33',
-            ])
+            ]),
+            'created_at' => '2024-01-22 20:11:13',
+            'updated_at' => '2024-01-22 20:54:11',
         ]);
 
         Keterangansk::create([
-            'kode_sk' => 140,
             'id_kodesk' => 2,
+            'kode_sk' => '400.3.5.6',
             'detail_keterangansk' => json_encode([
                 'no_sk' => 'E8',
                 'warga' => [
@@ -57,7 +59,7 @@ class KeteranganskTableSeeder extends Seeder
                         'alamat' => 'F18',
                         'jenis_pekerjaan' => 'F16',
                         'agama' => 'F17',
-                    ],
+                    ]
                 ],
                 'tanggal' => 'J28',
                 'ttd_kepala' => 'G33',
@@ -66,7 +68,209 @@ class KeteranganskTableSeeder extends Seeder
             'keterangan' => json_encode([
                 'keterangan_1' => 'F22',
                 'keterangan_2' => 'F23',
-            ])
+            ]),
+            'created_at' => '2024-01-22 20:11:13',
+            'updated_at' => '2024-01-24 00:43:33',
+        ]);
+
+        Keterangansk::create([
+            'id_kodesk' => 3,
+            'kode_sk' => '474.3',
+            'detail_keterangansk' => json_encode([
+                'no_sk' => 'C4',
+                'warga' => [
+                    [
+                        'nik' => null,
+                        'nama_warga' => 'C9',
+                        'jenis_kelamin' => 'C10',
+                        'tempat_lahir' => null,
+                        'tanggal_lahir' => null,
+                        'alamat' => 'C12',
+                        'jenis_pekerjaan' => null,
+                        'agama' => null,
+                        'umur' => 'C11'
+                    ],
+                    [
+                        'nik' => null,
+                        'nama_warga' => null,
+                        'jenis_kelamin' => null,
+                        'tempat_lahir' => null,
+                        'tanggal_lahir' => null,
+                        'alamat' => null,
+                        'jenis_pekerjaan' => null,
+                        'agama' => null,
+                        'umur' => null
+                    ]
+                ],
+                'tanggal' => 'D26',
+                'jabatan' => 'C27',
+                'ttd_pengaju' => null,
+                'ttd_kepala' => 'C31',
+            ]),
+            'keterangan' => json_encode([
+                'keterangan_1' => 'C18',
+                'keterangan_2' => 'C19',
+                'keterangan_3' => 'C20',
+                'keterangan_4' => 'C21',
+                'keterangan_5' => null,
+                'keterangan_6' => null,
+                'keterangan_7' => null,
+                'keterangan_8' => null,
+                'keterangan_9' => null,
+                'keterangan_10' => null,
+            ]),
+            'created_at' => '2024-01-22 20:16:43',
+            'updated_at' => '2024-01-22 20:45:01',
+        ]);
+
+        Keterangansk::create([
+            'id_kodesk' => 4,
+            'kode_sk' => '400.8.2.7',
+            'detail_keterangansk' => json_encode([
+                'no_sk' => 'E7',
+                'warga' => [
+                    [
+                        'nik' => 'E19',
+                        'nama_warga' => 'E13',
+                        'jenis_kelamin' => 'E15',
+                        'tempat_lahir' => 'E14',
+                        'tanggal_lahir' => 'I14',
+                        'alamat' => 'E20',
+                        'jenis_pekerjaan' => 'E17',
+                        'agama' => 'E18',
+                        'umur' => null,
+                    ],
+                    [
+                        'nik' => 'E29',
+                        'nama_warga' => 'E23',
+                        'jenis_kelamin' => 'E25',
+                        'tempat_lahir' => 'E24',
+                        'tanggal_lahir' => 'I24',
+                        'alamat' => 'E30',
+                        'jenis_pekerjaan' => 'E27',
+                        'agama' => 'E28',
+                        'umur' => null,
+                    ]
+                ],
+                'tanggal' => 'M40',
+                'jabatan' => 'J41',
+                'ttd_pengaju' => null,
+                'ttd_kepala' => 'J46',
+            ]),
+            'keterangan' => json_encode([
+                'keterangan_1' => 'E35',
+                'keterangan_2' => null,
+                'keterangan_3' => null,
+                'keterangan_4' => null,
+                'keterangan_5' => null,
+                'keterangan_6' => null,
+                'keterangan_7' => null,
+                'keterangan_8' => null,
+                'keterangan_9' => null,
+                'keterangan_10' => null,
+            ]),
+            'created_at' => '2024-01-22 20:53:48',
+            'updated_at' => '2024-01-22 22:09:17',
+        ]);
+
+        Keterangansk::create([
+            'id_kodesk' => 5,
+            'kode_sk' => '400',
+            'detail_keterangansk' => json_encode([
+                'no_sk' => 'G7',
+                'warga' => [
+                    [
+                        'nik' => 'E19',
+                        'nama_warga' => 'E13',
+                        'jenis_kelamin' => 'E15',
+                        'tempat_lahir' => 'E14',
+                        'tanggal_lahir' => 'G14',
+                        'alamat' => 'E20',
+                        'jenis_pekerjaan' => 'E17',
+                        'agama' => 'E18',
+                        'umur' => null,
+                    ],
+                    [
+                        'nik' => null,
+                        'nama_warga' => null,
+                        'jenis_kelamin' => null,
+                        'tempat_lahir' => null,
+                        'tanggal_lahir' => null,
+                        'alamat' => null,
+                        'jenis_pekerjaan' => null,
+                        'agama' => null,
+                        'umur' => null,
+                    ]
+                ],
+                'tanggal' => 'K32',
+                'jabatan' => 'H33',
+                'ttd_pengaju' => null,
+                'ttd_kepala' => 'H38',
+            ]),
+            'keterangan' => json_encode([
+                'keterangan_1' => 'A24',
+                'keterangan_2' => null,
+                'keterangan_3' => null,
+                'keterangan_4' => null,
+                'keterangan_5' => null,
+                'keterangan_6' => null,
+                'keterangan_7' => null,
+                'keterangan_8' => null,
+                'keterangan_9' => null,
+                'keterangan_10' => null,
+            ]),
+            'created_at' => '2024-01-23 23:27:31',
+            'updated_at' => '2024-01-23 23:34:00',
+        ]);
+
+        Keterangansk::create([
+            'id_kodesk' => 6,
+            'kode_sk' => '331',
+            'detail_keterangansk' => json_encode([
+                'no_sk' => 'E7',
+                'warga' => [
+                    [
+                        'nik' => 'E19',
+                        'nama_warga' => 'E13',
+                        'jenis_kelamin' => 'E15',
+                        'tempat_lahir' => 'E14',
+                        'tanggal_lahir' => 'G14',
+                        'alamat' => 'E20',
+                        'jenis_pekerjaan' => 'E17',
+                        'agama' => 'E18',
+                        'umur' => null,
+                    ],
+                    [
+                        'nik' => null,
+                        'nama_warga' => null,
+                        'jenis_kelamin' => null,
+                        'tempat_lahir' => null,
+                        'tanggal_lahir' => null,
+                        'alamat' => null,
+                        'jenis_pekerjaan' => null,
+                        'agama' => null,
+                        'umur' => null,
+                    ]
+                ],
+                'tanggal' => 'J32',
+                'jabatan' => 'G33',
+                'ttd_pengaju' => null,
+                'ttd_kepala' => 'G38',
+            ]),
+            'keterangan' => json_encode([
+                'keterangan_1' => 'F26',
+                'keterangan_2' => null,
+                'keterangan_3' => null,
+                'keterangan_4' => null,
+                'keterangan_5' => null,
+                'keterangan_6' => null,
+                'keterangan_7' => null,
+                'keterangan_8' => null,
+                'keterangan_9' => null,
+                'keterangan_10' => null,
+            ]),
+            'created_at' => '2024-01-23 23:43:52',
+            'updated_at' => '2024-01-23 23:51:59',
         ]);
     }
 }
--- database/seeders/KodeskTableSeeder.php
+++ database/seeders/KodeskTableSeeder.php
@@ -15,31 +15,96 @@ class KodeskTableSeeder extends Seeder
      */
     public function run()
     {
-        Kodesk::create([
-            'kode_sk' => 510,
-            'jenis_sk' => 'Surat Keterangan Usaha',
-            'singkatan_sk'=>'SKU',
-            'keterangan_kodesk' => json_encode([
-                'keterangan_1'=>'Jenis Usaha',
-                'keterangan_2' => 'Alamat Usaha',
-            ]),
-            'jumlah_warga' => 1,
-            'file_name' => 'sku.xlsx',
-            'url_print' => "plugin\xls\sku.xlsx"
+        {
+            Kodesk::create([
+                'kode_sk' => '503',
+                'jenis_sk' => 'Surat Keterangan Usaha',
+                'singkatan_sk' => 'SKU',
+                'keterangan_kodesk' => json_encode([
+                    'keterangan_1' => 'Jenis Usaha',
+                    'keterangan_2' => 'Alamat Usaha',
+                ]),
+                'jumlah_warga' => 1,
+                'file_name' => 'sku.xlsx',
+                'url_print' => 'plugin\xls\sku.xlsx',
+                'created_at' => '2024-01-22 20:11:13',
+                'updated_at' => '2024-01-22 20:54:11',
+            ]);
+    
+            Kodesk::create([
+                'kode_sk' => '400.3.5.6',
+                'jenis_sk' => 'Surat Keterangan Keluarga Miskin',
+                'singkatan_sk' => 'SKKM',
+                'keterangan_kodesk' => json_encode([
+                    'keterangan_1' => 'Keterangan Surat',
+                    'keterangan_2' => 'Usulan Surat',
+                ]),
+                'jumlah_warga' => 1,
+                'file_name' => 'skkm.xlsx',
+                'url_print' => 'plugin\xls\skkm.xlsx',
+                'created_at' => '2024-01-22 20:11:13',
+                'updated_at' => '2024-01-24 00:43:33',
+            ]);
+    
+            Kodesk::create([
+                'kode_sk' => '474.3',
+                'jenis_sk' => 'Surat Kematian',
+                'singkatan_sk' => 'SKM',
+                'keterangan_kodesk' => json_encode([
+                    'keterangan_1' => 'Hari',
+                    'keterangan_2' => 'Tanggal',
+                    'keterangan_3' => 'Di',
+                    'keterangan_4' => 'Disebabkan',
+                ]),
+                'jumlah_warga' => 1,
+                'file_name' => 'Surat Kematian.xlsx',
+                'url_print' => 'plugin\xls\Surat Kematian.xlsx',
+                'created_at' => '2024-01-22 20:16:43',
+                'updated_at' => '2024-01-22 20:16:43',
+            ]);
+    
+            Kodesk::create([
+                'kode_sk' => '400.8.2.7',
+                'jenis_sk' => 'Surat Keterangan Tidak Memiliki Buku Nikah',
+                'singkatan_sk' => 'SKBM',
+                'keterangan_kodesk' => json_encode([
+                    'keterangan_1' => 'Tahun Menikah',
+                ]),
+                'jumlah_warga' => 2,
+                'file_name' => 'Surat Keterangan Tidak memiliki buku nikahh.xlsx',
+                'url_print' => 'plugin\xls\Surat Keterangan Tidak memiliki buku nikahh.xlsx',
+                'created_at' => '2024-01-22 20:53:48',
+                'updated_at' => '2024-01-22 20:53:48',
+            ]);
+    
+            Kodesk::create([
+                'kode_sk' => '400',
+                'jenis_sk' => 'Surat Keterangan Domisili',
+                'singkatan_sk' => 'SKD',
+                'keterangan_kodesk' => json_encode([
+                    'keterangan_1' => 'Alamat',
+                ]),
+                'jumlah_warga' => 1,
+                'file_name' => 'Surat Keterangan Domisili.xlsx',
+                'url_print' => 'plugin\xls\Surat Keterangan Domisili.xlsx',
+                'created_at' => '2024-01-23 23:27:31',
+                'updated_at' => '2024-01-23 23:27:31',
+            ]);
+    
+            Kodesk::create([
+                'kode_sk' => '331',
+                'jenis_sk' => 'Surat Keterangan Catatan Kepolisian',
+                'singkatan_sk' => 'SKCK',
+                'keterangan_kodesk' => json_encode([
+                    'keterangan_1' => 'Keterangan Untuk',
+                ]),
+                'jumlah_warga' => 1,
+                'file_name' => 'Surat Keterangan Catatan Kepolisian.xlsx',
+                'url_print' => 'plugin\xls\Surat Keterangan Catatan Kepolisian.xlsx',
+                'created_at' => '2024-01-23 23:43:52',
+                'updated_at' => '2024-01-23 23:43:52',
+            ]);
 
-        ]);
-
-        Kodesk::create([
-            'kode_sk' => 140,
-            'jenis_sk' => 'Surat Keterangan Keluarga Miskin',
-            'singkatan_sk'=>'SKKM',
-            'keterangan_kodesk' => json_encode([
-                'keterangan_1'=>'Keterangan Surat',
-                'keterangan_2' => 'Usulan Surat',
-            ]),
-            'jumlah_warga' => 1,
-            'file_name' => 'skkm.xlsx',
-            'url_print' => "plugin\xls\skkm.xlsx"
-        ]);
+        }
     }
 }
